f26722fb5cca51a6da797e33faf542f9b94292d5
meganlukes1 <82395818+meganlukes@users.noreply.github.com>
2021-12-09 16:53:29 +0000

    Bamboo price calculator

--- main.py
+++ main.py
@@ -16,10 +16,40 @@
 #   print("You are at a healty weight")
 # else:
 #   print("You are underweight.")
-year = int(input("What year do you want to check? "))
-if year % 4 != 0:
-  print(f"{year} is not a leap year")
-elif (year % 100 == 0) and (year % 400 != 0):
-  print(f"{year} is not a leap year")
+# year = int(input("What year do you want to check? "))
+# if year % 4 != 0:
+#   print(f"{year} is not a leap year")
+# elif (year % 100 == 0) and (year % 400 != 0):
+#   print(f"{year} is not a leap year")
+# else:
+#   print(f"{year} is a leap year")
+print("Welcome to the bamboo garden!")
+lucky = input("Are you here to purchase lucky bamboo? Y or N ")
+if lucky != "N":
+  print("Get out of my nursery!")
 else:
-  print(f"{year} is a leap year")
+  size = int(input("That's good. Any serious collector knows lucky bamboo is not a true bamboo. What size bamboo do you want in gallons? 3, 7, or 15? "))
+  field_dug = input("Do you want field dug? It will grow faster than potted. Y or N ")
+  install = input("Do you want us to install it? Y or N ")
+  price = 0
+  if size == 3:
+    price = 35
+  elif size == 7:
+    price = 90
+  else:
+    price == 150
+  if field_dug == "Y":
+    if size == 3:
+      price = price + 15
+    elif size == 7:
+      price = price + 30
+    else:
+      price = price + 60
+  if install == "Y":
+    if size == 3:
+      price = price + 20
+    elif size == 7:
+      price = price + 30
+    else:
+      price = price + 60
+  print(f"That will be {price}. Yes, I know that's rather expensive, but think of it more like a tree.")
