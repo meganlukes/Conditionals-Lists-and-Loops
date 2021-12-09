921e01cd0451b394b9171edf53ff72eaf5ef8f28
meganlukes1 <82395818+meganlukes@users.noreply.github.com>
2021-12-09 17:19:07 +0000

    buzzfeed love calculator

--- main.py
+++ main.py
@@ -23,33 +23,49 @@
 #   print(f"{year} is not a leap year")
 # else:
 #   print(f"{year} is a leap year")
-print("Welcome to the bamboo garden!")
-lucky = input("Are you here to purchase lucky bamboo? Y or N ")
-if lucky != "N":
-  print("Get out of my nursery!")
-else:
-  size = int(input("That's good. Any serious collector knows lucky bamboo is not a true bamboo. What size bamboo do you want in gallons? 3, 7, or 15? "))
-  field_dug = input("Do you want field dug? It will grow faster than potted. Y or N ")
-  install = input("Do you want us to install it? Y or N ")
-  price = 0
-  if size == 3:
-    price = 35
-  elif size == 7:
-    price = 90
-  else:
-    price == 150
-  if field_dug == "Y":
-    if size == 3:
-      price = price + 15
-    elif size == 7:
-      price = price + 30
-    else:
-      price = price + 60
-  if install == "Y":
-    if size == 3:
-      price = price + 20
-    elif size == 7:
-      price = price + 30
-    else:
-      price = price + 60
-  print(f"That will be {price}. Yes, I know that's rather expensive, but think of it more like a tree.")
+# print("Welcome to the bamboo garden!")
+# lucky = input("Are you here to purchase lucky bamboo? Y or N ")
+# if lucky != "N":
+#   print("Get out of my nursery!")
+# else:
+#   size = int(input("That's good. Any serious collector knows lucky bamboo is not a true bamboo. What size bamboo do you want in gallons? 3, 7, or 15? "))
+#   field_dug = input("Do you want field dug? It will grow faster than potted. Y or N ")
+#   install = input("Do you want us to install it? Y or N ")
+#   price = 0
+#   if size == 3:
+#     price = 35
+#   elif size == 7:
+#     price = 90
+#   else:
+#     price == 150
+#   if field_dug == "Y":
+#     if size == 3:
+#       price = price + 15
+#     elif size == 7:
+#       price = price + 30
+#     else:
+#       price = price + 60
+#   if install == "Y":
+#     if size == 3:
+#       price = price + 20
+#     elif size == 7:
+#       price = price + 30
+#     else:
+#       price = price + 60
+#   print(f"That will be {price}. Yes, I know that's rather expensive, but think of it more like a tree.")
+print("Welcome to the Love Calculator!")
+hisName = input("What is his name? ").lower()
+herName = input("What is her name? ").lower()
+allT = hisName.count("t") + herName.count("t")
+allR = hisName.count("r") + herName.count("r")
+allU = hisName.count("u") + herName.count("u")
+allE = hisName.count("e") + herName.count("e")
+allL = hisName.count("l") + herName.count("l")
+allO = hisName.count("o") + herName.count("o")
+allV = hisName.count("v") + herName.count("v")
+total = allT + allR + allU + allE + allL + allO + allV + allE
+print(f"Your score is {total}.")
+if (total > 90) or (total < 10):
+  print("You go together like coke and mentos! \n That doesn't sound like a good thing.")
+elif (total > 40) and (total < 60):
+  print("You are alright together.")
