d179647195dd4ea951aa2fdcd4daad792af56212
meganlukes1 <82395818+meganlukes@users.noreply.github.com>
2021-12-09 15:04:11 +0000

    New BMI calculator

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-number = int(input("Give me a number "))
-if number % 2 == 1:
-  print(f"{number} is odd")
+# number = int(input("Give me a number "))
+# if number % 2 == 1:
+#   print(f"{number} is odd")
+# else:
+#   print(f"{number} is even")
+print("Let's calculate your BMI")
+kgWeight = float(input("How much do you weigh (in pounds)? ")) * 0.45359237
+mHeight = float(input("How tall are you in inches? ")) * 2.54 / 100
+bmi = round(kgWeight / (mHeight * mHeight), 1)
+print("Your BMI is " + str(bmi) + ".")
+if bmi > 30:
+  print("You are obese")
+elif bmi > 24.9:
+  print("You are at overweight")
+elif bmi > 18.4:
+  print("You are at a healty weight")
 else:
-  print(f"{number} is even")
+  print("You are underweight.")
